3dcc1481b525b0f647474fd6e2fc36be87c7a90f
davindb <davindbhagaspati@gmail.com>
2024-01-24 09:32:15 +0700

    rollback

--- functions/api.js
+++ functions/api.js
@@ -1,175 +1,171 @@
 const express = require("express");
-const serverless = require("serverless-http");
 const path = require("path");
 const csvtojson = require("csvtojson");
 const { spawn } = require("child_process");
 
 const app = express();
-const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("App is running..");
+app.use(express.static(path.join(__dirname, "../dist")));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
-app.use("/.netlify/functions/api", router);
-module.exports.handler = serverless(app);
+app.post("/api/trx", async (req, res) => {
+  try {
+    const jsonData = await csvtojson().fromFile(
+      "src/assets/db/final_transactions.csv"
+    );
 
-// app.use(express.static(path.join(__dirname, "../dist")));
-// app.use(express.json());
+    let { purchase_date, customer_id, limit, selected_batch } = req.body;
 
-// router.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../dist/index.html"));
-// });
+    if (purchase_date && !/^\d{4}-(0[1-9]|1[0-2])$/.test(purchase_date)) {
+      return res.status(400).json({ error: "Invalid purchase_date format" });
+    }
 
-// router.post("/api/trx", async (req, res) => {
-//   try {
-//     const jsonData = await csvtojson().fromFile(
-//       "src/assets/db/final_transactions.csv"
-//     );
+    customer_id = customer_id ? String(customer_id) : null;
 
-//     let { purchase_date, customer_id, limit, selected_batch } = req.body;
+    limit = limit ? parseInt(limit, 10) : 5;
+    selected_batch = selected_batch ? parseInt(selected_batch, 10) : 1;
 
-//     if (purchase_date && !/^\d{4}-(0[1-9]|1[0-2])$/.test(purchase_date)) {
-//       return res.status(400).json({ error: "Invalid purchase_date format" });
-//     }
+    const filteredData = jsonData.filter((item) => {
+      return (
+        (!purchase_date || item.purchase_date.startsWith(purchase_date)) &&
+        (!customer_id || item.customer_id === customer_id)
+      );
+    });
 
-//     customer_id = customer_id ? String(customer_id) : null;
+    const sortedData = filteredData.sort((a, b) => {
+      return new Date(b.purchase_date) - new Date(a.purchase_date);
+    });
 
-//     limit = limit ? parseInt(limit, 10) : 5;
-//     selected_batch = selected_batch ? parseInt(selected_batch, 10) : 1;
+    const groupedData = [];
+    for (let i = 0; i < sortedData.length; i += limit) {
+      groupedData.push(sortedData.slice(i, i + limit));
+    }
 
-//     const filteredData = jsonData.filter((item) => {
-//       return (
-//         (!purchase_date || item.purchase_date.startsWith(purchase_date)) &&
-//         (!customer_id || item.customer_id === customer_id)
-//       );
-//     });
+    const adjustedIndex = selected_batch - 1;
 
-//     const sortedData = filteredData.sort((a, b) => {
-//       return new Date(b.purchase_date) - new Date(a.purchase_date);
-//     });
+    if (adjustedIndex >= 0 && adjustedIndex < groupedData.length) {
+      const selectedSubarray = groupedData[adjustedIndex];
 
-//     const groupedData = [];
-//     for (let i = 0; i < sortedData.length; i += limit) {
-//       groupedData.push(sortedData.slice(i, i + limit));
-//     }
+      const responseData = {
+        data: selectedSubarray.map((item) => item),
+        total_data: sortedData.length,
+        total_batch: groupedData.length,
+        total_data_in_batch: selectedSubarray.length,
+        current_batch: selected_batch,
+      };
 
-//     const adjustedIndex = selected_batch - 1;
+      res.json(responseData);
+    } else {
+      res.status(404).json({ error: "Selected subarray not found" });
+    }
+  } catch (error) {
+    console.error("Error reading CSV file:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
-//     if (adjustedIndex >= 0 && adjustedIndex < groupedData.length) {
-//       const selectedSubarray = groupedData[adjustedIndex];
+app.post("/api/predict_proba", (req, res) => {
+  try {
+    let {
+      customer_id,
+      latest_category_1,
+      latest_category_2,
+      latest_category_3,
+      latest_category_4,
+      latest_category_5,
+    } = req.body;
 
-//       const responseData = {
-//         data: selectedSubarray.map((item) => item),
-//         total_data: sortedData.length,
-//         total_batch: groupedData.length,
-//         total_data_in_batch: selectedSubarray.length,
-//         current_batch: selected_batch,
-//       };
+    if (!customer_id) {
+      return res.status(400).json({ error: "customer_id must be filled" });
+    }
 
-//       res.json(responseData);
-//     } else {
-//       res.status(404).json({ error: "Selected subarray not found" });
-//     }
-//   } catch (error) {
-//     console.error("Error reading CSV file:", error);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
+    customer_id = String(customer_id);
 
-// router.post("/api/predict_proba", (req, res) => {
-//   try {
-//     let {
-//       customer_id,
-//       latest_category_1,
-//       latest_category_2,
-//       latest_category_3,
-//       latest_category_4,
-//       latest_category_5,
-//     } = req.body;
+    const categoryMapping = {
+      Automotive: 0,
+      Beauty: 1,
+      "Books & Media": 2,
+      Clothing: 3,
+      Electronics: 4,
+      "Food & Grocery": 5,
+      Furniture: 6,
+      "Health & Wellness": 7,
+      "Home & Kitchen": 8,
+      "Jewelry & Accessories": 9,
+      "Office Supplies": 10,
+      "Pet Supplies": 11,
+      "Sports & Outdoors": 12,
+      "Toys & Games": 13,
+      "": 14,
+    };
 
-//     if (!customer_id) {
-//       return res.status(400).json({ error: "customer_id must be filled" });
-//     }
+    const features = JSON.stringify({
+      latest_category_1: [parseInt(categoryMapping[latest_category_1])],
+      latest_category_2: [parseInt(categoryMapping[latest_category_2])],
+      latest_category_3: [parseInt(categoryMapping[latest_category_3])],
+      latest_category_4: [parseInt(categoryMapping[latest_category_4])],
+      latest_category_5: [parseInt(categoryMapping[latest_category_5])],
+    });
 
-//     customer_id = String(customer_id);
+    const pythonProcess = spawn("python", [
+      "script.py",
+      JSON.stringify(features),
+    ]);
 
-//     const categoryMapping = {
-//       Automotive: 0,
-//       Beauty: 1,
-//       "Books & Media": 2,
-//       Clothing: 3,
-//       Electronics: 4,
-//       "Food & Grocery": 5,
-//       Furniture: 6,
-//       "Health & Wellness": 7,
-//       "Home & Kitchen": 8,
-//       "Jewelry & Accessories": 9,
-//       "Office Supplies": 10,
-//       "Pet Supplies": 11,
-//       "Sports & Outdoors": 12,
-//       "Toys & Games": 13,
-//       "": 14,
-//     };
+    let dataBuffer = "";
+    pythonProcess.stdout.on("data", (data) => {
+      dataBuffer += data.toString();
+    });
 
-//     const features = JSON.stringify({
-//       latest_category_1: [parseInt(categoryMapping[latest_category_1])],
-//       latest_category_2: [parseInt(categoryMapping[latest_category_2])],
-//       latest_category_3: [parseInt(categoryMapping[latest_category_3])],
-//       latest_category_4: [parseInt(categoryMapping[latest_category_4])],
-//       latest_category_5: [parseInt(categoryMapping[latest_category_5])],
-//     });
+    pythonProcess.stderr.on("data", (data) => {
+      console.error(`Error from Python script: ${data}`);
+      res.status(500).json({ error: "Internal Server Error" });
+    });
 
-//     const pythonProcess = spawn("python", [
-//       "script.py",
-//       JSON.stringify(features),
-//     ]);
+    pythonProcess.on("close", (code) => {
+      if (code === 0) {
+        try {
+          const result = JSON.parse(dataBuffer);
 
-//     let dataBuffer = "";
-//     pythonProcess.stdout.on("data", (data) => {
-//       dataBuffer += data.toString();
-//     });
+          const sortedIndices = Array.from(result.keys()).sort(
+            (a, b) => result[b] - result[a]
+          );
 
-//     pythonProcess.stderr.on("data", (data) => {
-//       console.error(`Error from Python script: ${data}`);
-//       res.status(500).json({ error: "Internal Server Error" });
-//     });
+          const sortedProba = sortedIndices.map((index) => result[index]);
 
-//     pythonProcess.on("close", (code) => {
-//       if (code === 0) {
-//         try {
-//           const result = JSON.parse(dataBuffer);
+          const categories = sortedIndices.map(
+            (index) => Object.keys(categoryMapping)[index]
+          );
 
-//           const sortedIndices = Array.from(result.keys()).sort(
-//             (a, b) => result[b] - result[a]
-//           );
+          const categoryObject = {};
+          categories.forEach((key, i) => {
+            categoryObject[key] = sortedProba[i];
+          });
 
-//           const sortedProba = sortedIndices.map((index) => result[index]);
+          res.json({
+            customer_id: customer_id,
+            recommendation: categoryObject,
+          });
+        } catch (parseError) {
+          console.error("Error parsing Python script output:", parseError);
+          res.status(500).json({ error: "Internal Server Error" });
+        }
+      } else {
+        console.error(`Python script exited with code ${code}`);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+  } catch (error) {
+    console.error("Error processing request:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
-//           const categories = sortedIndices.map(
-//             (index) => Object.keys(categoryMapping)[index]
-//           );
-
-//           const categoryObject = {};
-//           categories.forEach((key, i) => {
-//             categoryObject[key] = sortedProba[i];
-//           });
-
-//           res.json({
-//             customer_id: customer_id,
-//             recommendation: categoryObject,
-//           });
-//         } catch (parseError) {
-//           console.error("Error parsing Python script output:", parseError);
-//           res.status(500).json({ error: "Internal Server Error" });
-//         }
-//       } else {
-//         console.error(`Python script exited with code ${code}`);
-//         res.status(500).json({ error: "Internal Server Error" });
-//       }
-//     });
-//   } catch (error) {
-//     console.error("Error processing request:", error);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
+const PORT = process.env.PORT || 3700;
+app.listen(PORT, () => {
+  console.log(`Server running at https://terra-store-web.netlify.app/${PORT}/`);
+});
